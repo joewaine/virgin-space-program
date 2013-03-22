505fae4aed22a2e2b5bb2e44f6e2562247414ee0
Joseph Waine <joe.waine@gmail.com>
2013-03-22 14:27:49 -0400

    pm changes

--- app/models/seatbooking.rb
+++ app/models/seatbooking.rb
@@ -14,8 +14,6 @@ class Seatbooking < ActiveRecord::Base
   attr_accessible :user_id, :flight_id, :seat_id
   belongs_to :seat
   belongs_to :flight
-<<<<<<< HEAD
   belongs_to :user
-=======
->>>>>>> 1a718a8a10af2715b7a840de540ad0d90db83d82
+
 end
