d208f14b4ba23e0d1b659b00ca61810e218ca799
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-03-22 15:22:17 -0400

    login feature works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Virginspaceprogram::Application.routes.draw do
   root :to => 'home#index'
   get '/login' => 'session#new'
+  post '/login' => 'session#create'
 resources :flights, :planes, :seats, :seatbooking, :users
 
 end
