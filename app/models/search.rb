d227523b84408fb76ff7de5bd4bb32c54a7c26d7
Joseph Waine <joe.waine@gmail.com>
2013-03-23 15:52:54 -0400

    flight show page

---
+++ app/models/search.rb
@@ -0,0 +1,11 @@
+class Search
+  # def find_flights(origin, dest)
+  # selected_flights = []
+  # @flights = Flight.all
+  # @flights.each do |flight|
+  #   if flight.origin == origin && flight.destination == dest
+  #     selected_flights << flight
+  #   end
+  # end
+  # end
+end
