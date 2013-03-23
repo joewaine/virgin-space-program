8e32e916ba4ba504a8a9a01a7bd19d8afaa88d12
NickyH <nickyhughes00@gmail.com>
2013-03-23 18:36:41 -0400

    SHow grid

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,9 +8,6 @@ class FlightsController < ApplicationController
  def show
     @flight = Flight.find(params[:id])
     @plane = @flight.plane
-    # plane_id = @flight.plane_id
-    # flight = Flight.find(params[:id])
-    # @plane = Flight.where(:plane_id => plane_id)
 
  end
  def create
