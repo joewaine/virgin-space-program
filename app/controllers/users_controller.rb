f111ee6c3be29d1eac7e100c8b07ae0d29bbb2a6
Joseph Waine <joe.waine@gmail.com>
2013-03-23 13:32:40 -0400

    saturdabefore pull

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(params[:user])
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
